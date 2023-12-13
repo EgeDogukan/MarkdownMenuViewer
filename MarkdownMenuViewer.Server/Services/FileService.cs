64d27b1c5b55a985e7ee2d079ab34ebc59ed37e9
EgeDogukan <eseven20@ku.edu.tr>
2023-12-13 17:08:48 +0300

    wip/implementing FileService

---
+++ MarkdownMenuViewer.Server/Services/FileService.cs
@@ -0,0 +1,37 @@
+﻿using MarkdownMenuViewer.Server.Models;
+
+namespace MarkdownMenuViewer.Server.Services
+{
+    public class FileService : IFileService
+    {
+        public async Task<IEnumerable<DirectoryItem>> GetDirectoryContentsAsync(string path)
+        {
+            //check whether the directory in path variable exists
+            if(Directory.Exists(path) == false)
+            {
+                throw new DirectoryNotFoundException("The directory at " + path + "does not exist.");
+            }
+
+            //check whether the path is empty
+            if(path == null)
+            {
+                throw new ArgumentNullException(path);
+            }
+
+
+            var DirectoryItems = new List<DirectoryItem>();
+
+            return await Task.FromResult(DirectoryItems);
+        }
+
+        public async Task<MarkdownFile> GetMarkdownFileAsync(string path)
+        {
+
+
+            var MarkdownFile = new MarkdownFile();
+
+            return await Task.FromResult(MarkdownFile);
+        }
+
+    }
+}
