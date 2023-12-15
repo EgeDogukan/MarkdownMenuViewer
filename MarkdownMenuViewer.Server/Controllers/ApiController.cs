190afbf28737e319ceab6f02482f23782c75ef80
EgeDogukan <eseven20@ku.edu.tr>
2023-12-15 07:43:53 +0300

    feat: implemented ApiController, two httpget methods

---
+++ MarkdownMenuViewer.Server/Controllers/ApiController.cs
@@ -0,0 +1,47 @@
+﻿using MarkdownMenuViewer.Server.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarkdownMenuViewer.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileSystemController : ControllerBase
+    {
+
+        private readonly IFileService fileService;
+
+        public FileSystemController(IFileService fileService)
+        {
+            this.fileService = fileService;
+        }
+
+        [HttpGet("directory")]
+        public async Task<IActionResult> GetDirectoryContents(string path)
+        {
+            try
+            {
+                var contents = await fileService.GetDirectoryContentsAsync(path);
+                return Ok(contents);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet("file")]
+        public async Task<IActionResult> GetMarkdownFile(string path)
+        {
+            try
+            {
+                var file = await fileService.GetMarkdownFileAsync(path);
+                return Ok(file);
+            }
+            catch (FileNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
