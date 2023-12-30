8bcf74c48c154d1e989cb031e334231c3830fac5
Batuhan <emrebatuhangulec@hotmail.com>
2023-12-31 01:35:13 +0300

    added some files

--- markdownmenuviewer.client/src/main.jsx
+++ markdownmenuviewer.client/src/main.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-<<<<<<< HEAD
-
-=======
-import './index.css'
->>>>>>> 6352fa77b437bf140aace3814dc120e47006a498
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
